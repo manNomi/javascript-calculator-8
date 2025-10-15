bafbdb0899ea72eb74937f3662110e36820ec1b9
manNomi <hanmw110@naver.com>
2025-10-15 22:10:40 +0900

    feat: parse 함수를 구현하다

--- src/service/model/Parser.js
+++ src/service/model/Parser.js
@@ -1,1 +1,9 @@
-export default class Parser {}
+export default class Parser {
+  parseData(regxs, inputText) {
+    const regex = new RegExp(`[${regxs.join('')}]`);
+    return inputText
+      .split(regex)
+      .filter((item) => item !== '') // 빈문자열 제거
+      .map((item) => Number(item));
+  }
+}
