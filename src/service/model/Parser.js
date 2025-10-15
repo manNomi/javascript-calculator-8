dfa88c588b23c46afff6de14f420154c5f68602a
manNomi <hanmw110@naver.com>
2025-10-15 23:15:43 +0900

    test: parser usecase를 작성하다

--- src/service/model/Parser.js
+++ src/service/model/Parser.js
@@ -1,6 +1,7 @@
 export default class Parser {
   parseData(regxs, inputText) {
-    const regex = new RegExp(`[${regxs.join('')}]`);
+    const totalRegexs = [...regxs, ',', ':'];
+    const regex = new RegExp(`[${totalRegexs.join('')}]`);
     return inputText
       .split(regex)
       .filter((item) => item !== '') // 빈문자열 제거
--- test/Parser.test.js
+++ test/Parser.test.js
@@ -3,10 +3,39 @@ import Parser from '../src/service/model/Parser.js';
 describe('Parser 클래스를 테스트 하다', () => {
   const parser = new Parser();
   it('구분자와 입력을 받아 파싱을 하다', () => {
-    const regx = ['/', '|'];
-    const exInput = '/123/2|31';
-    expect(parser.parseData(regx, exInput)).toEqual([123, 2, 31]);
+    const regxs = ['/', '|'];
+    const inputText = '/123/2|31';
+    expect(parser.parseData(regxs, inputText)).toEqual([123, 2, 31]);
   });
 });
 
 // issue 객체나 배열은 toEqual을 사용해야함...
+
+describe('Parser 클래스의 useCase를 추가하다', () => {
+  const parser = new Parser();
+
+  it('구분자는 기본적으로 , : 를 포함합니다', () => {
+    const regxs = [];
+    const inputText = '11:6,1';
+    expect(parser.parseData(regxs, inputText)).toEqual([11, 6, 1]);
+  });
+
+  it('구분자가 숫자인경우', () => {
+    const regxs = ['1', '2', '3', '#'];
+    const inputText = '1142232#61';
+    expect(parser.parseData(regxs, inputText)).toEqual([4, 6]);
+  });
+  it('구분자에 \\ 이스케이프가 포함되는경우', () => {
+    const regxs = ['//\\n', '\\'];
+    const inputText = '//\\n1//\\n2//\\n3';
+    expect(parser.parseData(regxs, inputText)).toEqual([1, 2, 3]);
+  });
+  it('구분자가 여러글자 인경우', () => {
+    const regxs = ['ab', 'bc', 'cd'];
+    const inputText = 'ab1bc2cd3';
+    expect(parser.parseData(regxs, inputText)).toEqual([1, 2, 3]);
+  });
+});
+
+// issue \ 처리가 매우 까다로움
+// \\ 의 경우에는
