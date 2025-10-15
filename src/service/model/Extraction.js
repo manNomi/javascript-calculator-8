3f7793e6abe453a09af53f7226a50427e94839fd
manNomi <hanmw110@naver.com>
2025-10-15 23:46:45 +0900

    test: test코드의 함수에 input 매개변수를 추가하다

---
+++ src/service/model/Extraction.js
@@ -0,0 +1,3 @@
+export default class Extraction {
+  extractCustom(inputText) {}
+}
--- test/Extraction.test.js
+++ test/Extraction.test.js
@@ -4,11 +4,13 @@ import Extraction from '../src/service/model/Extraction';
 
 describe('Extraction 클래스 테스트', () => {
   it('//ㅌ\\n 사이의 커스텀 구분자 ㅌ을 추출한다', () => {
+    const inputText = 'e//ㅌ\\neasd123';
     const extraction = new Extraction();
-    expect(extraction.extractCustom()).toEqual(['ㅌ']);
+    expect(extraction.extractCustom(inputText)).toEqual(['ㅌ']);
   });
   it('커스텀 구분자가 없다면 빈값으로 반환된다', () => {
+    const inputText = 'eneasd123';
     const extraction = new Extraction();
-    expect(extraction.extractCustom()).toEqual([]);
+    expect(extraction.extractCustom(inputText)).toEqual([]);
   });
 });
