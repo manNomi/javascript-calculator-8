18b811702800e4d9c95f7366d911b5759cdc66ac
manNomi <hanmw110@naver.com>
2025-10-15 22:10:40 +0900

    feat: parse 함수를 구현하다

--- test/Parser.test.js
+++ test/Parser.test.js
@@ -1,10 +1,12 @@
-import Parser from '../src/service/model/Parser';
+import Parser from '../src/service/model/Parser.js';
 
 describe('Parser 클래스를 테스트 하다', () => {
   const parser = new Parser();
   it('구분자와 입력을 받아 파싱을 하다', () => {
     const regx = ['/', '|'];
     const exInput = '/123/2|31';
-    expect(parser.parseData(regx, exInput)).toBe([123, 2, 31]);
+    expect(parser.parseData(regx, exInput)).toEqual([123, 2, 31]);
   });
 });
+
+// issue 객체나 배열은 toEqual을 사용해야함...
