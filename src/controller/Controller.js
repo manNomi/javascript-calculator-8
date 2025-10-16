953a6b1ab78424c57fe97e0f15771eda8cfb270c
manNomi <hanmw110@naver.com>
2025-10-16 20:44:53 +0900

    feat: contooler 클래스의 run함수를 구현하다

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,9 +1,21 @@
 import Extraction from '../service/model/Extraction.js';
 import Parser from '../service/model/Parser.js';
+import inputView from '../view/InputView.js';
+import outputView from '../view/OutputView.js';
+import Number from '../service/model/Number.js';
 
 export default class Controller {
   constructor() {
-    const extraction = new Extraction();
-    const parser = new Parser();
+    this.extraction = new Extraction();
+    this.parser = new Parser();
+  }
+
+  async run() {
+    const input =
+      await inputView.readLineMessage('덧셈할 문자열을 입력해 주세요.');
+    const customRegexs = this.extraction.extractCustom(input);
+    const parsedNumber = this.parser.parseData(customRegexs, input);
+    const numbers = new Number(parsedNumber);
+    outputView.printMessage(`결과 : ${numbers.getAddedNumbers()}`);
   }
 }
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,7 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const inputView = {
-  async readLineMesage(message) {
+  async readLineMessage(message) {
     const input = await Console.readLineAsync(message);
     return input;
   },
