abe8e8ec15c8593cd032f4e1960da84e1f8ca08b
manNomi <hanmw110@naver.com>
2025-10-17 23:31:50 +0900

    test: 공백을 파싱하는 테스트코드를 작성하다

--- test/Parser.test.js
+++ test/Parser.test.js
@@ -41,6 +41,11 @@ describe('Parser 클래스의 useCase를 추가하다', () => {
     expect(parser.parseData([], inputText)).toEqual([-1, 2, 3]);
   });
 
+  it('공백이 포함되는 경우', () => {
+    const inputText = '-1,,3';
+    expect(parser.parseData([], inputText)).toEqual([-1, 0, 3]);
+  });
+
   it('커스텀 구분자가 포함이 되는경우 //;\\n1', () => {
     const regxs = [';'];
     const inputText = '//;\\n1';
