2f8b73d5d3f794a05670475168a92e9591f637d6
manNomi <hanmw110@naver.com>
2025-10-18 00:31:27 +0900

    fix: parser가 커스텀구분자를 추출하지 못하던 버그를 수정하다

--- src/service/model/Parser.js
+++ src/service/model/Parser.js
@@ -1,9 +1,17 @@
 export default class Parser {
-  parseData(regxs, inputText) {
-    const totalRegexs = [...regxs, ',', ':'];
+  parseData(regexs, inputText) {
+    let inputResult = inputText;
+
+    // 커스텀 구분자가 포함되는경우 양식대로 파싱
+    regexs.forEach((regex) => {
+      const splitType = `//${regex}\\n`;
+      inputResult = inputText.split(splitType).join('');
+    });
+
+    const totalRegexs = [...regexs, ',', ':'];
     const regex = new RegExp(`[${totalRegexs.join('')}]`);
 
-    return inputText
+    return inputResult
       .split(regex)
       .filter((item) => item !== '') // 빈문자열 제거
       .map((item) => Number(item));
