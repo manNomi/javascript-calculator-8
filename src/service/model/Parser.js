953f26d8b562db240621822a56173596042069c4
manNomi <hanmw110@naver.com>
2025-10-18 17:18:13 +0900

    fix: 커스텀 구분자가 여러개일때 파싱안되던 버그를 수정하다

--- src/service/model/Parser.js
+++ src/service/model/Parser.js
@@ -5,17 +5,12 @@ export default class Parser {
     // 커스텀 구분자가 포함되는경우 양식대로 파싱
     regexs.forEach((regex) => {
       const splitType = `//${regex}\\n`;
-      inputResult = inputText.split(splitType).join('');
+      inputResult = inputResult.split(splitType).join('');
     });
 
     const totalRegexs = [...regexs, ',', ':'];
     const regex = new RegExp(`[${totalRegexs.join('')}]`);
 
-    console.log(
-      'inputResult',
-      inputResult,
-      inputResult.split(regex).filter((item) => item !== ''),
-    );
     return inputResult
       .split(regex)
       .filter((item) => item !== '') // 빈문자열 제거
